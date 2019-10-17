581374e4dc959ae8fc12d97cafc97216f04c5722
Vladimir Zinovev <test@test.ru>
2019-10-17 13:33:59 +0300

    relise 10172019

---
+++ ShipBattle/Models/ComputerModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShipBattle.Conntrollers;
+using System.Configuration;
+
+namespace ShipBattle.Models
+{
+    public class ComputerModel : PlayerController, PlayerModel
+    {
+
+        private List<int[]> fleet = new List<int[]>();
+        public readonly string name;
+        public readonly bool canPlayGame;
+
+        public ComputerModel()
+        {
+            this.name = "Computer";
+            this.canPlayGame = true;
+        }
+
+        public void addToFleet(ShipModel ship = null)
+        {
+            int shipCount = int.Parse(ConfigurationManager.AppSettings["ShipCount"]);
+            Random rand = new Random();
+            for (int i = 0; i < shipCount; i++)
+            {
+                var sideValues = Enum.GetValues(typeof(ShipModel.vectorSide));
+
+                ShipModel model = new ShipModel(rand.Next(0, BoardModel.Xsize), rand.Next(0, BoardModel.Ysize), shipCount, (ShipModel.vectorSide)sideValues.GetValue(rand.Next(sideValues.Length)));
+                this.fleet.AddRange(model.vectorShip);
+            }
+        }
+    }
+}
---
+++ ShipBattle/Models/HumanModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using ShipBattle.Conntrollers;
+
+namespace ShipBattle.Models
+{
+    public class HumanModel : PlayerController, PlayerModel
+    {
+        private List<int[]> fleet = new List<int[]>();
+        public readonly string name;
+        public readonly bool canPlayGame;
+
+        public HumanModel(string name)
+        {
+            this.name = name;
+            this.canPlayGame = true;
+        }
+
+        public void addToFleet(ShipModel ship = null)
+        {
+            this.fleet.AddRange(ship.vectorShip);
+        }
+    }
+}
